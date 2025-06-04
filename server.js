c3ef57763a53d15ef7eeae8579afa8563fa4ccd9
Shaikh Zahid <muhammedshaikhzahidu@MUHAMMEDs-MacBook-Air.local>
2025-06-04 17:09:57 +0300

    first commit

--- server.js
+++ server.js
@@ -10,20 +10,10 @@ const PORT = process.env.PORT || 3001;
 app.use(cors());
 app.use(express.json());
 
-const allowedOrigins = ['https://your-frontend.netlify.app',"http://localhost:5173/"];
-
 app.use(cors({
-    origin: function (origin, callback) {
-        // Allow requests with no origin (like mobile apps, curl, etc.)
-        if (!origin) return callback(null, true);
-        if (allowedOrigins.includes(origin)) {
-            return callback(null, true);
-        } else {
-            return callback(new Error('Not allowed by CORS'));
-        }
-    },
-    credentials: true
+    origin: '*'
 }));
+
 
 
 // Routes
